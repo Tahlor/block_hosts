44088f6e32cca9e8ad9bd655f8206feee6172a70
Taylor <tahlor@gmail.com>
2022-11-07 14:54:18 -0700

    update message

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # block_hosts
 Block websites using the hosts file on Linux
 
+    block 0 - nothing
+    block 1 - block Twitter/linkedin
+    block 2 - block news/shopping
+    block 3 - block email
+
+    # unblock
+    unblock 2 - this is the same as block 1
+    we're not really in a block/unblock paradigm anymore, so much as a block-level paradigm
+
 ## Set cron job
     sudo crontab -e
     
--- block.py
+++ block.py
@@ -11,6 +11,12 @@ from time import sleep
 * Run from Windows Python not supported/tested
 * WSL block/unblock working with WUDO
 
+## Levels
+Block Level:
+0: Nothing blocked
+1: Twitter & Linked In
+2: Everything but Email
+3: Also Email
 
 """
 
@@ -34,11 +40,11 @@ def read(path):
         content = f.read()
     return content
 
-linux_prefix = read("./linux_default").format(socket.gethostname())
-windows_prefix = read("./windows_default").format(socket.gethostname())
+linux_prefix = read("./websites/linux_default").format(socket.gethostname())
+windows_prefix = read("./websites/windows_default").format(socket.gethostname())
 
 
-def get_sites(website_file="websites.txt"):
+def get_sites(website_file):
     with (root / website_file).open("r") as f:
         return f.read()
 
@@ -64,7 +70,7 @@ def set_globals(linux=True):
         else:
             HOSTS_FILE_PATH=r"C:\Windows\System32\drivers\etc\hosts"
         block_sites=block_sites_windows
-        prefix=read("./windows_default").format(socket.gethostname())
+        prefix=read("./websites/windows_default").format(socket.gethostname())
         print(HOSTS_FILE_PATH)
 
 def windows_flush():
@@ -75,9 +81,9 @@ def windows_flush():
     command=r"""/mnt/c/Windows/System32/cmd.exe \c "cd C:\Users\tarchibald && ipconfig /flushdns > FLUSH_RESULT" """
     subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 
-def block_sites_linux():
+def block_sites_linux(level=2):
     print("blocking sites...")
-    websites = get_sites().split()
+    websites = get_sites_by_level(level, block=True)
     #print(websites)
     print(f"blocking {HOSTS_FILE_PATH}")
     with Path(HOSTS_FILE_PATH).open("w") as f:
@@ -88,9 +94,10 @@ def block_sites_linux():
                 f.write("127.0.0.1 {} \n".format(w))
                 f.write("127.0.0.1 www.{} \n".format(w))
 
-def block_sites_windows(): 
+def block_sites_windows(level=2): 
     print("blocking sites...")
-    websites = get_sites().split()
+    websites = get_sites_by_level(level, block=True)
+
     print(f"blocking {HOSTS_FILE_PATH}")
     # temp_path="./hosts.tmp"
     
@@ -111,9 +118,38 @@ def block_sites_windows():
     windows_flush()
 
 
-def unblock_sites():
+def whatamidoing(level, block):
+    blocking = ["Nothing", "Twitter/LinkedIn", "News/Shopping", "Email"]
+    print(f"Target Level: {level}")
+
+    if block:
+        print(f"You are blocking {' + '.join(blocking[:level+1])}")
+    else:
+        print(f"You are UNblocking {' + '.join(blocking[level:])}")
+
+
+def get_sites_by_level(level=2, block=True):
+    """ block (bool): True: include lower levels (0,1,2...); if block=level2, also block level1
+                      False: include higher levels (4,3,...); if unblock level2, also unblock level3
+    """
+    whatamidoing(level,block)
+    websites = []
+    for source in Path("./websites/").rglob("level*"):
+        src_level = source.stem[-1]
+        if src_level.isnumeric():
+            src_level = int(src_level)
+            # Unblock: only return sites at a lower block level than the current one
+            if (src_level < level and not block) or (src_level <= level and block):
+                print(f"Adding {source}")
+                websites += get_sites(source).split()
+    return websites
+
+def unblock_sites(level=2):
+    """ Recreate hosts block file from scratch
+    """
     print("unblocking sites...")
-    websites = get_sites("always_block_websites.txt").split()
+    websites = get_sites_by_level(level, block=False)
+
     with Path( HOSTS_FILE_PATH).open("w") as f:
         f.write(prefix)
         for w in websites:
@@ -168,13 +204,13 @@ def unblock_one(item="youtube"):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, error = process.communicate()
 
-def unblock_timer(duration=5):
+def unblock_timer(duration=5, level=2):
     break_message = "You got a {} minute break!".format(duration)
     try:
     # Allow user to end break early
         os.system('spd-say "{}"'.format(break_message))
         input(break_message)
-        unblock_sites()
+        unblock_sites(level)
 
         sleeper(duration)
     except:
@@ -201,6 +237,7 @@ def n2w(n):
 
 def parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument('level', nargs='?', default=2)
     parser.add_argument('--unblock', action="store_true")
     parser.add_argument('--unblock_all', action="store_true")
     parser.add_argument('--block', action="store_true")
@@ -214,42 +251,42 @@ def parser():
 
     opts = parser.parse_args()
     set_globals()
-    
+    opts.level = int(opts.level)
     break_message = "You got a {} minute break!"
     if opts.unblock_all:
         unblock_all()
     elif opts.unblock:
-        unblock_sites()
+        unblock_sites(opts.level)
     elif opts.break_mode is not None:
-        unblock_timer()
+        unblock_timer(level=opts.level)
         while True:
-                block_sites()
+                block_sites(opts.level)
                 os.system('spd-say "Blocking websites"')
                 print("Blocking sites for {} minutes".format(opts.break_mode))
                 sleeper(opts.break_mode)
                 os.system('spd-say "{}"'.format(break_message))
-                unblock_timer()
+                unblock_timer(level=opts.level)
     elif opts.lunch is not None:
         unblock_timer(30)
-        block_sites()
+        block_sites(opts.level)
         os.system('spd-say "Blocking websites"')
 
     elif opts.off:
-        unblock_sites()
+        unblock_sites(opts.level)
         remove_from_cron()
     elif opts.on:
-        block_sites()
+        block_sites(opts.level)
         install_block_to_cron(opts.user)
     elif opts.block:
-        block_sites()
+        block_sites(opts.level)
     elif opts.site:
         unblock_one(opts.site)
     elif opts.youtube is not None:
         unblock_one(opts.youtube)
     elif opts.unblock:
-        unblock_sites()
+        unblock_sites(opts.level)
     else:
-        block_sites()
+        block_sites(opts.level)
 
 
 if __name__ == '__main__':
