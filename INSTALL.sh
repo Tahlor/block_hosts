51e272d1cac3f7ee56c0b63432cbfa7a0d000e62
tahlor <tahlor@gmail.com>
2024-03-10 21:52:05 -0600

    probably messed up everything

--- INSTALL.sh
+++ INSTALL.sh
@@ -5,10 +5,10 @@ LOCAL_USER=${1-$(logname)}
 
 # 59 23 * * * python3 /home/taylor/bashrc/ext/block_hosts/block.py --on --user taylor > /home/taylor/bashrc/ext/block_hosts/BLOCK.log 2>&1 # PERSISTENT
 
-UNBLOCK="0 * * * * python3 /home/$LOCAL_USER/bashrc/ext/block_hosts/block.py --unblock >> /home/$LOCAL_USER/bashrc/ext/block_hosts/BLOCK.log 2>&1"
-BLOCK="5 * * * * python3 /home/$LOCAL_USER/bashrc/ext/block_hosts/block.py --block >> /home/$LOCAL_USER/bashrc/ext/block_hosts/BLOCK.log 2>&1"
+UNBLOCK="0 * * * * python3 /home/$LOCAL_USER/bashrc/ext/block_hosts/block_hosts/block.py --unblock >> /home/$LOCAL_USER/bashrc/ext/block_hosts/block_hosts/BLOCK.log 2>&1"
+BLOCK="5 * * * * python3 /home/$LOCAL_USER/bashrc/ext/block_hosts/block_hosts/block.py --block >> /home/$LOCAL_USER/bashrc/ext/block_hosts/block_hosts/BLOCK.log 2>&1"
 crontab_file=$(sudo crontab -l)
-echo "$crontab_file" > "/home/$LOCAL_USER/bashrc/ext/block_hosts/backup_cron"
+echo "$crontab_file" > "/home/$LOCAL_USER/bashrc/ext/block_hosts/block_hosts/backup_cron"
 echo "$crontab_file" | grep -F "${BLOCK:10:100}" # ignore exact timing or if commented out etc.
 
 # If grep doesn't find it
